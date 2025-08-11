fd31cc194745bcbf8befabd32806492efdfc5941
Faheem <seebiz.gpt0623.dev56@gmail.com>
2025-08-11 13:33:02 +0500

    animation is done

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -11,30 +11,31 @@ const Hero = () => {
           "linear-gradient(359.63deg, #F5F5FA 0.34%, #8F95BB 12.27%, #1A1A28 22.13%, #1A1A28 51.41%, #1A1A28 81.96%, #1A1A28 99.69%)",
       }}
       className="w-full h-full">
-      <div className="w-full h-full flex justify-center items-center flex-col">
+      <div className="w-full h-full flex justify-center items-center flex-col relative">
         <Navbar />
         <div className="w-full h-full max-w-[1200px] md:mt-[128px] mt-[60px]">
           <BackgroundBeamsWithCollision>
-          <Text as="h1" className="mb-[50px] text-[#F5F5FA] text-center">
-            Hire 6 AI Voice Employees for{" "}
-              {" "}
-              Less Than $1/Hour
-          </Text>
-          <Text as ="h2" className="text-[#F5F5FA] text-center mb-10 md:mb-[140px] font-normal md:leading-[45px]">
-            <span className=" font-bold">
-              {" "}
-              Kognitiiv.ai
-            </span>{" "}
-            gives your business a full team of AI voice agents that handle
-            sales, support, marketing, and more <br className="md:block hidden" /> 24/7, no breaks, no burnout
-          </Text>
-          <Text className="md:text-[24px] text-[20px] font-medium text-[#F5F5FA] text-center">
-            Be among the first to access this game-changing workforce. <br className="md:block hidden" />Join the
-            waitlist to claim early access + exclusive launch perks
-          </Text>
+            <Text as="h1" className="mb-[50px] text-[#F5F5FA] text-center">
+              Hire 6 AI Voice Employees for Less Than $1/Hour
+            </Text>
+            <Text
+              as="h2"
+              className="text-[#F5F5FA] text-center mb-10 md:mb-[140px] font-normal md:leading-[45px]">
+              <span className=" font-bold"> Kognitiiv.ai</span> gives your
+              business a full team of AI voice agents that handle sales,
+              support, marketing, and more <br className="md:block hidden" />{" "}
+              24/7, no breaks, no burnout
+            </Text>
+            <Text className="md:text-[24px] text-[20px] font-medium text-[#F5F5FA] text-center">
+              Be among the first to access this game-changing workforce.{" "}
+              <br className="md:block hidden" />
+              Join the waitlist to claim early access + exclusive launch perks
+            </Text>
           </BackgroundBeamsWithCollision>
         </div>
+        <div className="">
           <GoogleGeminiEffectAnimation />
+        </div>
       </div>
     </div>
   );
